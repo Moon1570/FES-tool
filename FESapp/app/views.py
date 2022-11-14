03be42e06b06ac774aa23b2936b63df1e026d82f
Moon1570 <moon.cse4.bu@gmail.com>
2022-11-14 08:59:52 +0600

    copilot tested

--- FESapp/app/views.py
+++ FESapp/app/views.py
@@ -5,8 +5,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name':'Moon'})
 
-def add(request):
-    val1 = int(request.POST['num1'])
-    val2 = int(request.POST['num2'])
-    res = val1+val2
-    return render(request, 'result.html', {'res':res})
