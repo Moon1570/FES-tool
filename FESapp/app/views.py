d3221271f8529cac84dbeae9c0b65bfc18248996
Moon1570 <moon.cse4.bu@gmail.com>
2022-11-21 05:03:18 +0600

    got value & json response

--- FESapp/app/views.py
+++ FESapp/app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name':'Moon'})
 
+def calc(request):
+    #get name from request
+    name = request.POST.get('name')
+    weight = int(request.POST.get('weight'))
+    print(weight)
+    intervalTime = int(request.POST.get('intervalTime'))
+
+    #print to console
+    print(name)
+    print(weight)
+    print(intervalTime)
+
+
+
+    return JsonResponse({'name':name, 'weight':weight, 'intervalTime':intervalTime})
